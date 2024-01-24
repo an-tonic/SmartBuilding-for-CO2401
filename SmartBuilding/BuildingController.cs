e8e48e213decf27ed553e54321676b13fad4d966
Antoniy Serdyukov <ASerdyukov@uclan.ac.uk>
2024-01-24 16:02:22 +0200

    Level 1 reqirements complete

--- SmartBuilding/BuildingController.cs
+++ SmartBuilding/BuildingController.cs
@@ -5,10 +5,13 @@
 
         string buildingID;
         string currentState;
+        string[] validStates = { "open", "closed", "out_of_hours", "fire_drill", "fire_alarm" };
 
-        public BuildingController(string buildingID)
+
+        public BuildingController(string buildingID = "", string state = "")
         {
-            this.buildingID = buildingID;
+            this.buildingID = buildingID.ToLower();
+            this.currentState = state.ToLower();
         }
 
         public string GetBuildingID()
@@ -16,6 +19,28 @@
             return buildingID;
         }
 
+        public string GetCurrentState()
+        {
+            return currentState;
+        }
 
+        public void SetBuildingID(string buildingID)
+        {
+            this.buildingID = buildingID.ToLower();
+        }
+
+        public bool SetCurrentState(string state)
+        {
+            if (validStates.Contains(state))
+            {
+                this.currentState = state;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
     }
 }
--- SmartBuildingTests/BuildingControllerTests.cs
+++ SmartBuildingTests/BuildingControllerTests.cs
@@ -8,10 +8,8 @@ namespace SmartBuildingTests
     public class BuildingControllerTests
     {
 
-
-
         [Test]
-        public void L1R1_L1R2_BuildingControllerConstructor_SetBuildingID()
+        public void L1R1_L1R2_Constructor_SetBuildingID()
         {
 
             //Arrenge
@@ -26,14 +24,118 @@ namespace SmartBuildingTests
             Assert.AreEqual(newBuildingID, returnedBuildingID);
         }
 
-       
+        [Test]
+        public void L1R3_Constructor_SetsBuildingIDToLowercase()
+        {
+
+            //Arrenge
+            string shouldReturnID = "test";
+            var controller = new BuildingController("TEST");
 
 
-        //naming convention for your test method names: MethodBeingTested_TestScenario_ExpectedOutput
-        //E.g. SetCurrentState_InvalidState_ReturnsFalse
+            //Act
+            string returnedBuildingID = controller.GetBuildingID();
 
-   
+            //Assert
+            Assert.AreEqual(shouldReturnID, returnedBuildingID);
+        }
 
+        [Test]
+        public void L1R4_SetBuildingID_BuildingIDToLowercase()
+        {
+
+            //Arrenge
+            string shouldReturnID = "test1";
+            var controller = new BuildingController("TEST");
+
+
+            //Act
+            controller.SetBuildingID("TEST1");
+            string returnedBuildingID = controller.GetBuildingID();
+
+            //Assert
+            Assert.AreEqual(shouldReturnID, returnedBuildingID);
+        }
+
+        [Test]
+        public void L1R5_L1R6_Constructor_SetCurrentStateToOutOfHours()
+        {
+
+            //Arrenge
+            
+            string newCurrentState = "out_of_hours";
+            var controller = new BuildingController(state:newCurrentState);
+
+
+            //Act
+            string returnedState = controller.GetCurrentState();
+
+            //Assert
+            Assert.AreEqual(newCurrentState, returnedState);
+        }
+
+
+
+        [TestCase("out_of_hours")]
+        [TestCase("closed")]
+        [TestCase("open")]
+        [TestCase("fire_drill")]
+        [TestCase("fire_alarm")]
+        public void L1R7_CheckValidCurrentState_ReturnTrue(string testState)
+        {
+
+            //Arrenge
+            var controller = new BuildingController();
+
+
+            //Act
+            bool returnedState = controller.SetCurrentState(testState);
+
+            //Assert
+            Assert.IsTrue(returnedState);
+        }
+
+
+        [TestCase("invalid")]
+        [TestCase("")]
+
+        public void L1R7_CheckInvalidCurrentState_ReturnFalse(string testState)
+        {
+
+            //Arrenge
+            var controller = new BuildingController();
+
+
+            //Act
+            bool returnedState = controller.SetCurrentState(testState);
+
+            //Assert
+            Assert.IsFalse(returnedState);
+        }
+
+
+        [TestCase("out_of_hours")]
+        [TestCase("closed")]
+        [TestCase("open")]
+        [TestCase("fire_drill")]
+        [TestCase("fire_alarm")]
+        public void L1R7_SetCurrentStateToValidState_ReturnTrue(string testState)
+        {
+
+            //Arrenge
+            var controller = new BuildingController();
+
+
+            //Act
+            controller.SetCurrentState(testState);
+            string returnedState = controller.GetCurrentState();
+
+            //Assert
+            Assert.AreEqual(returnedState, testState);
+        }
+
+
+      
 
     }
 }
